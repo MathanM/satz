ace0f133da8b62f38c70b0d9af1b3c90d0f58017
Mathan Mathialagan <mathan.m@mathanm-ltmnjdj.internal.salesforce.com>
2025-01-13 17:33:21 +0530

    Add tag carousel

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {ButtonComponent} from '../../components/button/button.component';
 import {BadgeComponent} from '../../components/badge/badge.component';
 import {WorkCardComponent} from '../../components/work-card/work-card.component';
@@ -11,8 +11,7 @@ import {WorkCardComponent} from '../../components/work-card/work-card.component'
     WorkCardComponent
   ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
